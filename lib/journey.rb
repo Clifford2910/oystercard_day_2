3ca77be592909c88434c69b161ce8e36fe200ca8
liatmoss <liatmoss@hotmail.com>
2019-10-22 16:54:30 +0100

    created Journey class

---
+++ lib/journey.rb
@@ -0,0 +1,22 @@
+require_relative 'oystercard'
+require_relative 'station'
+
+class Journey
+
+  def start_journey
+
+  end
+
+  def finish_journey
+
+  end
+
+  def fare
+
+  end
+
+  def complete?
+
+  end
+
+end
---
+++ spec/journey_spec.rb
@@ -0,0 +1,11 @@
+require 'journey'
+
+describe Journey do
+  subject(:journey) { described_class.new }
+  let(:station) { double :station, zone: 1}
+
+
+  
+
+
+end
